ef406edbeeb1cf2cf7caa3046d8876d401ffc638
Uddhav <uddhav3114@gmail.com>
2026-04-19 16:47:58 -0400

    Update app_config.dart

---
+++ lib/app_config.dart
@@ -0,0 +1,11 @@
+class AppConfig {
+  static const quizApiKey = String.fromEnvironment('QUIZ_API_KEY');
+
+  static void validate() {
+    if (quizApiKey.isEmpty) {
+      throw Exception(
+        'Missing QUIZ_API_KEY. Run with --dart-define=QUIZ_API_KEY=YOUR_API_KEY',
+      );
+    }
+  }
+}
